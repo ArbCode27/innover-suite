273378c5911d8b9cdb1d2a1f0b05c404aee32f0d
abraham <code.arb2712@gmail.com>
2026-09-09 11:49:58 -0400

    boton claro oscuro en el menu

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { AppThemeProvider } from "@/components/theme/app-theme-provider";
 import { PaletteProvider } from "@/lib/theme/palette-provider";
 import { configureZodSpanish } from "@/lib/validation/zod-es";
 
@@ -15,13 +15,13 @@ type AppProvidersProps = {
 
 export const AppProviders = ({ children }: AppProvidersProps) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <AppThemeProvider>
       <PaletteProvider>
         <TooltipProvider delayDuration={200}>
           {children}
           <Toaster />
         </TooltipProvider>
       </PaletteProvider>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 };
